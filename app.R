23779b1046bbb88fc8fb43c2b4e25945dc9e46e0
Daniel Schmidtke <56757699+DaSchmidtke@users.noreply.github.com>
2020-02-16 12:36:05 +0100

    Update app.R

--- app.R
+++ app.R
@@ -1,9 +1,11 @@
 #install.packages("shiny")
 #install.packages("shinyWidgets")
+#install.packages("shinyjs")
 #install.packages("ggplot2")
 #install.packages("jpeg")
 library(shiny)
 library(shinyWidgets)
+library(shinyjs)
 library(ggplot2)
 library(jpeg)
 
